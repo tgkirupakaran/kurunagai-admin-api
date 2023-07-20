3ee658d2ff257e40912563e95a1178ef01142b1c
kirupa <kirupa@geakminds.com>
2023-07-19 22:02:37 -0400

    Update redisConfig.js

--- config/redisConfig.js
+++ config/redisConfig.js
@@ -4,7 +4,8 @@ const redisOptions={
     host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT,
     username: process.env.REDIS_USER,
-    password: process.env.REDIS_PASSWORD
+    password: process.env.REDIS_PASSWORD,
+    ssl: True
 }
 
 module.exports = redisOptions
